933dba07038c7ef32b2981c55af5918dae51f13c
Sean <seanchew88@gmail.com>
2024-12-01 20:32:30 -0700

    now working!
    changed zoom
    
    changed zoom

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([51, -114], 11);
+
+
+var map = L.map('map').setView([51, -114], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
